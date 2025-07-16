39e99ad168b28545eec03fbbc1aa1952aceaf76d
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-07-15 21:02:49 -0300

    rota curso

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const cursoRoutes = require('./routes/cursoRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/cursos', cursoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
