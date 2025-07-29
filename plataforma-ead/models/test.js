488e7b3dc01f8a79ccdce2487627fbc2559b3293
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-07-29 19:29:21 -0300

    test js

--- plataforma-ead/models/test.js
+++ plataforma-ead/models/test.js
@@ -1,16 +1,18 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Test extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Test.belongsTo(models.Module, {
+        foreignKey: 'moduleId',
+        as: 'module'
+      });
+
+      Test.hasMany(models.Question, {
+        foreignKey: 'testId',
+        as: 'questions',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Test.init({
