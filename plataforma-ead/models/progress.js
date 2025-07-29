ba489c283493bb7185c93df7076681faa065db0d
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-07-29 19:30:58 -0300

    progress js

--- plataforma-ead/models/progress.js
+++ plataforma-ead/models/progress.js
@@ -1,16 +1,27 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Progress extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Progress.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user'
+      });
+
+      Progress.belongsTo(models.Course, {
+        foreignKey: 'courseId',
+        as: 'course'
+      });
+
+      Progress.belongsTo(models.Module, {
+        foreignKey: 'moduleId',
+        as: 'module'
+      });
+
+      Progress.belongsTo(models.Lesson, {
+        foreignKey: 'lessonId',
+        as: 'lesson'
+      });
     }
   }
   Progress.init({
