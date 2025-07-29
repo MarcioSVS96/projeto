772757b2c3f1b72b418e47d83ba3de645fa63e26
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-07-29 19:29:05 -0300

    lesson js

--- plataforma-ead/models/lesson.js
+++ plataforma-ead/models/lesson.js
@@ -1,16 +1,22 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Lesson extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Lesson.belongsTo(models.Module, {
+        foreignKey: 'moduleId',
+        as: 'module'
+      });
+
+      Lesson.hasMany(models.Comment, {
+        foreignKey: 'lessonId',
+        as: 'comments'
+      });
+
+      Lesson.hasMany(models.Progress, {
+        foreignKey: 'lessonId',
+        as: 'progresses'
+      });
     }
   }
   Lesson.init({
