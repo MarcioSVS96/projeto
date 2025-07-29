15e6a235bd22a78c7686911022ebfc7531f88dc7
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-07-29 19:30:39 -0300

    certificate js

--- plataforma-ead/models/certificate.js
+++ plataforma-ead/models/certificate.js
@@ -1,16 +1,17 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Certificate extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Certificate.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user'
+      });
+
+      Certificate.belongsTo(models.Course, {
+        foreignKey: 'courseId',
+        as: 'course'
+      });
     }
   }
   Certificate.init({
