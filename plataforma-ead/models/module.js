ee566fd4c6514e326177f242027e2213053e0ab1
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-07-29 19:28:49 -0300

    module js

--- plataforma-ead/models/module.js
+++ plataforma-ead/models/module.js
@@ -1,16 +1,29 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Module extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Module.belongsTo(models.Course, {
+        foreignKey: 'courseId',
+        as: 'course'
+      });
+
+      Module.hasMany(models.Lesson, {
+        foreignKey: 'moduleId',
+        as: 'lessons',
+        onDelete: 'CASCADE'
+      });
+
+      Module.hasOne(models.Test, {
+        foreignKey: 'moduleId',
+        as: 'test',
+        onDelete: 'CASCADE'
+      });
+
+      Module.hasMany(models.Progress, {
+        foreignKey: 'moduleId',
+        as: 'progresses'
+      });
     }
   }
   Module.init({
